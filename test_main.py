9257854a643ecba60a6f3c57640860b610f8ee0d
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-19 17:59:42 +0000

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,15 @@
+import unittest
+from main import *
+
+class UnitTests(unittest.TestCase) :
+    def test_graph(self) :
+        fighand=plt.gca()
+        figdat = fighand.get_lines()[0].get_xydata()
+        this_x, this_y = zip(*figdat)
+        self.assertTrue( len(this_y)==9, "you do not have the correct number of data points in your graph" )
+        filedata = np.loadtxt("md_results.txt")
+        for i in range(len(this_x) ) :
+            tmid = ( filedata[i,0] + filedata[i+1,0] ) / 2
+            self.assertTrue( np.abs(tmid - this_x[i])<1E-6 )
+            tmid = ( filedata[i+1,1] - filedata[i,1] ) / ( filedata[i+1,0] - filedata[i,0], "one or more of the temperatures on the x-axis of your graph are incorrect" )
+            self.assertTrue( np.abs(tmid - this_y[i])<1E-6, "one or more of the heat capacities you have calculated is incorrect" )
